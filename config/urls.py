01386bfa2d24370157bd20ddb9469321b3b3802e
PetrakovVladyslav <petrakov.14ca114@gmail.com>
2025-11-17 13:04:09 +0200

    зÐpre valid

--- config/urls.py
+++ config/urls.py
@@ -18,13 +18,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path
 from django.views.i18n import set_language
 
 urlpatterns = [
     # path('', home_view, name='home'),  # Добавляем главную страницу в основные паттерны
-    path("admin/", admin.site.urls),
+    #    path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("apps.core.urls")),
     path("", include("apps.banner.urls")),
